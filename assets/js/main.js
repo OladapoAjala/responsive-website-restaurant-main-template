625447a4cfacdcb8d66421b32ce14b758006c296
OladapoAjala <ajalaoladapoemmanuel.ao@gmail.com>
2021-01-06 15:36:50 +0100

    v1 of template for responsive-website-restaurant-main-template

---
+++ assets/js/main.js
@@ -0,0 +1,74 @@
+/*==================== SHOW MENU ====================*/
+const showMenu = (toggleId, navId) => {
+
+}
+
+showMenu('','')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('')
+
+function linkAction() {
+
+}
+
+navLink.forEach(n => n.addEventListener('', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('')
+
+function scrollActive() {
+
+}
+
+window.addEventListener('', )
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+
+}
+
+window.addEventListener('', )
+
+/*==================== SHOW SCROLL TOP ====================*/ 
+function scrollTop() {
+
+}
+
+window.addEventListener('', )
+
+/*==================== DARK LIGHT THEME ====================*/ 
+const themeButtom =
+const darkTheme =
+const iconTheme =
+
+// Previously selected topic (if user selected)
+const selectedTheme =
+const selectedIcon =
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = 
+const getCurrentIcon =
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('', () => {
+    // Add or remove the dark / icon theme
+
+    // We save the theme and the current icon that the user chose
+})
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+
+});
+
+sr.reveal(``
+
+    , {
+
+})
